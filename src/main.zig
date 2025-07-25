41a531385bc8060faadd728658dda744161a8ab1
joaofsoares <joaofsoares@yahoo.com>
2025-07-25 11:16:18 +0100

    convert from C to ZIG

--- src/main.zig
+++ src/main.zig
@@ -41,14 +41,14 @@ pub fn main() !void {
 
     raylib.SetTargetFPS(60);
 
-    const bar: Bar = .{
+    var bar: Bar = .{
         .position = .{ .x = WIDTH / 2 - 50, .y = HEIGHT - 100 },
         .width = 100.0,
         .height = 20.0,
         .color = raylib.GREEN,
     };
 
-    const ball: Ball = .{
+    var ball: Ball = .{
         .position = .{ .x = WIDTH / 2, .y = HEIGHT - 125 },
         .radius = 10.0,
         .color = raylib.WHITE,
@@ -58,25 +58,88 @@ pub fn main() !void {
     const bricks = try create_rectangles(allocator);
     defer bricks.deinit();
 
+    var start_pressed = false;
+    var win = false;
+    var remaining_bricks: u32 = BRICK_COUNT;
+    var collision_brick: bool = false;
+    var collision_bar: bool = false;
+
     while (!raylib.WindowShouldClose()) {
+        if (raylib.IsKeyPressed(raylib.KEY_SPACE) and !start_pressed) {
+            start_pressed = true;
+        }
+
+        if (raylib.IsKeyDown(raylib.KEY_RIGHT) and bar.position.x < WIDTH - bar.width) {
+            bar.position.x += 10;
+        }
+
+        if (raylib.IsKeyDown(raylib.KEY_LEFT) and bar.position.x > 0) {
+            bar.position.x -= 10;
+        }
+
+        for (0..BRICK_COUNT) |i| {
+            collision_brick = raylib.CheckCollisionCircleRec(ball.position, ball.radius, raylib.Rectangle{ .x = bricks.items[i].position.x, .y = bricks.items[i].position.y, .width = bricks.items[i].width, .height = bricks.items[i].height });
+
+            if (collision_brick and bricks.items[i].is_active) {
+                bricks.items[i].is_active = false;
+                remaining_bricks -= 1;
+                ball_speed.y *= -1.0;
+                if (ball.position.x < bricks.items[i].position.x or
+                    ball.position.x > bricks.items[i].position.x + bricks.items[i].width)
+                {
+                    ball_speed.x *= -1.0;
+                }
+            }
+        }
+
+        collision_bar = raylib.CheckCollisionCircleRec(ball.position, ball.radius, raylib.Rectangle{ .x = bar.position.x, .y = bar.position.y, .width = bar.width, .height = bar.height });
+
+        if (collision_bar) {
+            if (ball.position.y + ball.radius >= bar.position.y) {
+                ball_speed.y *= -1.0;
+            }
+            if (ball.position.x < bar.position.x or ball.position.x > bar.position.x + bar.width) {
+                ball_speed.x *= -1.0;
+            }
+        }
+
+        if (!win and start_pressed) {
+            ball.position.x += ball_speed.x;
+            ball.position.y += ball_speed.y;
+
+            if ((ball.position.x >= (WIDTH - ball.radius)) or
+                (ball.position.x <= ball.radius))
+            {
+                ball_speed.x *= -1.0;
+            }
+
+            if ((ball.position.y >= (HEIGHT - ball.radius)) or
+                (ball.position.y <= ball.radius))
+            {
+                ball_speed.y *= -1.0;
+            }
+        }
+
         raylib.BeginDrawing();
 
         raylib.ClearBackground(raylib.BLACK);
 
         for (bricks.items) |brick| {
-            const p_x: i32 = @intFromFloat(brick.position.x);
-            const p_y: i32 = @intFromFloat(brick.position.y);
-            const p_width: i32 = @intFromFloat(brick.width);
-            const p_height: i32 = @intFromFloat(brick.height);
-
             if (brick.is_active) {
-                raylib.DrawRectangle(p_x, p_y, p_width, p_height, brick.color);
+                raylib.DrawRectangle(@intFromFloat(brick.position.x), @intFromFloat(brick.position.y), @intFromFloat(brick.width), @intFromFloat(brick.height), brick.color);
             }
         }
 
         raylib.DrawCircleV(ball.position, ball.radius, ball.color);
 
-        raylib.DrawRectangle(bar.position.x, bar.position.y, bar.width, bar.height, bar.color);
+        raylib.DrawRectangle(@intFromFloat(bar.position.x), @intFromFloat(bar.position.y), @intFromFloat(bar.width), @intFromFloat(bar.height), bar.color);
+
+        if (remaining_bricks == 0) {
+            win = true;
+            const message_size: u32 = @intCast(@divTrunc(raylib.MeasureText("You win!", 20), 2));
+            const message_calc_x: c_int = @intCast(WIDTH / 2 - message_size);
+            raylib.DrawText("You win!", message_calc_x, HEIGHT / 2 - 10, 20, raylib.GREEN);
+        }
 
         raylib.EndDrawing();
     }
